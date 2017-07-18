c91cbfa15e0c8800e171f931b099238a3d14b167
ringolip <zhangringo@gmail.com>
2017-07-18 14:46:47 +0800

    reply

--- wechat.py
+++ wechat.py
@@ -3,6 +3,7 @@ import web
 import os
 import hashlib
 import time
+from lxml import etree
 
 class WechatInterface:
     def __init__(self):
@@ -27,7 +28,15 @@ class WechatInterface:
         if hashcode == signature:
             return echostr
 
-
+    def POST(self):
+        str_xml = web.data()#接收的数据
+        xml = etree.fromstring(str_xml)
+        msgType = xml.find('MsgType').text
+        fromUser = xml.find('FromUserName').text
+        toUser = xml.find('ToUserName').text
+        if msgType == 'text':
+            content = xml.find('Content').text
+            return self.render.reply(fromUser, toUser, int(time.time()), content)
 
 
 
